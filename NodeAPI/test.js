9c0d4dd44f354a6f80187cbe0516fea2e0a32862
LauraSw <56430448+LauraSw@users.noreply.github.com>
2020-07-25 15:15:17 +0200

    Neues Repository

---
+++ NodeAPI/test.js
@@ -0,0 +1,7 @@
+"use strict";
+var L06_NodeAPI;
+(function (L06_NodeAPI) {
+    console.log("halloo");
+    console.log(process.env.PORT);
+})(L06_NodeAPI || (L06_NodeAPI = {}));
+//# sourceMappingURL=test.js.map
---
+++ NodeAPI/test.ts
@@ -0,0 +1,4 @@
+namespace L06_NodeAPI {
+    console.log("halloo");
+    console.log(process.env.PORT);
+}
